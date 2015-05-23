2fae950cffdb3a662a3dcbb742dac0932c8eae2d
Julxzs <julzso23@gmail.com>
2015-05-23 16:30:34 +0100

    Added main.lua
    Contains basic event functions for loveframes to function

---
+++ main.lua
@@ -0,0 +1,31 @@
+function love.load(args)
+	loveframes = require("lib.loveframes")
+end
+
+function love.update(dt)
+	loveframes.update(dt)
+end
+
+function love.draw()
+	loveframes.draw()
+end
+
+function love.mousepressed(x, y, button)
+	loveframes.mousepressed(x, y, button)
+end
+
+function love.mousereleased(x, y, button)
+	loveframes.mousereleased(x, y, button)
+end
+
+function love.keypressed(key, unicode)
+	loveframes.keypressed(key, unicode)
+end
+
+function love.keyreleased(key)
+	loveframes.keyreleased(key)
+end
+
+function love.textinput(text)
+	loveframes.textinput(text)
+end

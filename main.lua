0c58d3ca95ed874791a489bce18cbec648be3974
Julxzs <julzso23@gmail.com>
2015-06-09 00:08:46 +0100

    Test environment for ship and asteroid

--- main.lua
+++ main.lua
@@ -1,21 +1,41 @@
 function love.load(args)
 	loveframes = require("lib.loveframes")
+
+	require('modules.images')
+
+	local Ship = require('modules.ship')
+
+	ship = Ship:new(love.window.getWidth() / 2, love.window.getHeight() / 2)
+
+	local Asteroid = require('modules.asteroid')
+
+	asteroid = Asteroid:new(512, 512, 16)
 end
 
 function love.update(dt)
 	loveframes.update(dt)
+
+	asteroid:update()
 end
 
 function love.draw()
+	ship:draw()
+
+	asteroid:draw()
+
 	loveframes.draw()
 end
 
 function love.mousepressed(x, y, button)
 	loveframes.mousepressed(x, y, button)
+
+	asteroid:mouseDown(button)
 end
 
 function love.mousereleased(x, y, button)
 	loveframes.mousereleased(x, y, button)
+
+	asteroid:mouseUp(x, y, button)
 end
 
 function love.keypressed(key, unicode)
